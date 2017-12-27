8b63ee88d15b3c713de7f334fa6e19bb1ef27c3f
sianniello <steno87@hotmail.com>
2017-12-27 23:09:31 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/booking", methods=['POST'])
+def new_booking():
+    booking_json = request.get_json()
+    print(booking_json)
+    return "OK"
