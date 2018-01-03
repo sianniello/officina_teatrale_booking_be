0937f9606e7d5542ca0f7fd5271947a1dd26ad3b
sianniello <steno87@hotmail.com>
2018-01-03 16:35:49 +0100

    return

--- db_connector.py
+++ db_connector.py
@@ -2,31 +2,31 @@ from pymongo import MongoClient
 
 
 ROWS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Z']
-COLUMNS = [[13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13],
-           [13, 16, 13]
+COLUMNS = [[6, 10, 6],
+           [10, 14, 9],
+           [11, 14, 11],
+           [11, 15, 11],
+           [11, 14, 11],
+           [13, 15, 13],
+           [13, 14, 13],
+           [15, 14, 15],
+           [15, 15, 15],
+           [15, 14, 15],
+           [16, 15, 16],
+           [15, 14, 15],
+           [15, 15, 15],
+           [13, 14, 13],
+           [13, 15, 13],
+           [12, 14, 12],
+           [12, 15, 12],
+           [11, 15, 11],
+           [11, 14, 11],
+           [11, 15, 11],
+           [11, 14, 11],
+           [11, 15, 11],
+           [12, 14, 12],
+           [12, 14, 11],
+           [12, 13, 12]
            ]
 # URL = "localhost:27017"
 URL = "mongodb://steno87:110203_Ca@cluster0-shard-00-00-fxpno.mongodb.net:27017,cluster0-shard-00-01-fxpno.mongodb.net:27017,cluster0-shard-00-02-fxpno.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
@@ -40,21 +40,21 @@ bookings = db.bookings
 
 def seats_initialize(rows=ROWS, columns=COLUMNS):
     for idx, i in enumerate(rows):
-        for j in range(1, columns[idx][0]):
+        for j in range(0, columns[idx][0]):
             seat = {
-                "seat_id": i + "S" + str(j),
+                "seat_id": i + "S" + str(j + 1),
                 "status": "available"
             }
             seats.insert_one(seat)
-        for j in range(1, columns[idx][1]):
+        for j in range(0, columns[idx][1]):
             seat = {
-                "seat_id": i + "C" + str(j),
+                "seat_id": i + "C" + str(j + 1),
                 "status": "available"
             }
             seats.insert_one(seat)
-        for j in range(1, columns[idx][2]):
+        for j in range(0, columns[idx][2]):
             seat = {
-                "seat_id": i + "D" + str(j),
+                "seat_id": i + "D" + str(j + 1),
                 "status": "available"
             }
             seats.insert_one(seat)
